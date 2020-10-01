fix(ringmap): Handle case where ringmap stack has wrong shape

bondia/plot/ringmap.py
@@ -233,12 +233,21 @@ class RingMapPlot(HeatMapPlot, Reader):
             rm_stack = ccontainers.RingMap.from_file(
                 self._stack_path, freq_sel=sel_freq
             )
-            rm_stack = np.squeeze(rm_stack.map[sel_beam, sel_pol, sel_freq])
-            if self.polarization == self.mean_pol_text:
-                rm_stack = np.nanmean(rm_stack, axis=0)
+            try:
+                rm_stack = np.squeeze(rm_stack.map[sel_beam, sel_pol, sel_freq])
+            except IndexError as err:
+                logger.error(
+                    f"map dataset of ringmap stack file "
+                    f"is missing [{sel_beam}, {sel_pol}, {sel_freq}] (beam, polarization, "
+                    f"frequency). map has shape {rm_stack.map.shape}:\n{err}"
+                )
+                self.template_subtraction = False
+            else:
+                if self.polarization == self.mean_pol_text:
+                    rm_stack = np.nanmean(rm_stack, axis=0)
 
-            # FIXME: this is a hack. remove when rinmap stack file fixed.
-            rmap -= rm_stack.reshape(rm_stack.shape[0], -1, 2).mean(axis=-1)
+                # FIXME: this is a hack. remove when rinmap stack file fixed.
+                rmap -= rm_stack.reshape(rm_stack.shape[0], -1, 2).mean(axis=-1)
 
         if self.crosstalk_removal:
             # The mean of an all-nan slice (masked?) is nan. We don't need a warning about that.