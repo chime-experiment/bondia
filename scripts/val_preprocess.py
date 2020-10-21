d0818e73d4f8fa3caa38d179fbd68b45b9000846
Rick Nitsche <rick@phas.ubc.ca>
2020-10-21 14:09:59 -0700

    feat(val_preprocess): add preprocessing script

---
+++ scripts/val_preprocess.py
@@ -0,0 +1,92 @@
+import logging
+import glob
+import os
+import sys
+
+from pathlib import Path
+
+from ch_pipeline.core import containers as ccontainers
+from draco.core import containers
+
+d = "/project/rpp-krs/chime/chime_processed/daily"
+out_dir = "/project/rpp-krs/chime/chime_processed/validation_preprocess"
+force = False
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def process(path, name, file_name, file_out_name, container, **kwargs):
+    in_file = glob.glob(os.path.join(path, file_name))
+    if not in_file:
+        logger.info(f"Found 0 {name} files in {path} (Expected 1).")
+        return
+    elif len(in_file) > 1:
+        logger.info(f"Found {len(in_file)} {name} files in {path} (Expected 1).")
+        sys.exit(1)
+
+    in_file = in_file[0]
+
+    lsd_from_filename = int(os.path.splitext(os.path.basename(in_file))[0][-4:])
+    if lsd != lsd_from_filename:
+        logger.error(
+            f"Found file for LSD {lsd} when expecting LSD {lsd_from_filename}: {in_file}"
+        )
+        sys.exit(1)
+
+    full_out_dir = os.path.join(out_dir, rev, str(lsd))
+
+    Path(full_out_dir).mkdir(parents=True, exist_ok=True)
+    out_file = os.path.join(full_out_dir, f"{file_out_name}_{lsd}.h5")
+    if os.path.isfile(out_file) and not force:
+        logger.debug(
+            f"Skipping {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
+        )
+        return
+    logger.info(
+        f"Processing {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
+    )
+
+    rm = container.from_file(in_file, **kwargs)
+    rm.to_disk(out_file)
+
+
+rev_dirs = glob.glob(os.path.join(d, "rev_*"))
+for rev_dir in rev_dirs:
+    if not os.path.isdir(rev_dir):
+        logger.debug(f"Skipping {rev_dir} because it's not a (revision) directory.")
+        continue
+    rev = os.path.split(rev_dir)[-1]
+
+    lsd_dirs = sorted(glob.glob(os.path.join(rev_dir, "*")))
+
+    index = {}
+    for lsd_dir in lsd_dirs:
+        try:
+            lsd = int(os.path.basename(lsd_dir))
+        except ValueError as err:
+            logger.debug(f"Skipping dir {lsd_dir}: {err}")
+        else:
+            if rev not in index:
+                index[rev] = {}
+            if lsd in index[rev]:
+                logger.error(f"Tried to add {lsd} twice")
+                sys.exit(1)
+
+            process(
+                lsd_dir,
+                "ringmap",
+                "ringmap_lsd_*.h5",
+                f"ringmap_validation_freqs_lsd",
+                ccontainers.RingMap,
+                freq_sel=slice(400, 401),
+                pol_sel=[0, 3],
+            )
+            process(
+                lsd_dir,
+                "sensitivity",
+                "sensitivity_lsd_*.h5",
+                f"sensitivity_validation_lsd",
+                containers.SystemSensitivity,
+                pol_sel=[0, 2],
+            )
