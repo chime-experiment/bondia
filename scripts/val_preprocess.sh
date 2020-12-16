b9826d5eb757eea7126bdbdfe09c18fe439a3414
Rick Nitsche <rick@phas.ubc.ca>
2020-12-16 13:33:21 -0800

    increase preprocessing script memory

--- scripts/val_preprocess.sh
+++ scripts/val_preprocess.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 # number of MPI processes
 #SBATCH --cpus-per-task=1 # number of OpenMP processes
-#SBATCH --mem=1G # memory per node
+#SBATCH --mem=4G # memory per node
 #SBATCH --time=0-04:00:00
 #SBATCH --job-name=chp/validation-preprocessing
 
