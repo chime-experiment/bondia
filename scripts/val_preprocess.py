a788e167a64f8efd924b91373338fdb8e14c1c55
Rick Nitsche <rick@phas.ubc.ca>
2020-12-16 14:03:15 -0800

    val_preprocess: only run if there are files to process

--- scripts/val_preprocess.py
+++ scripts/val_preprocess.py
@@ -1,3 +1,4 @@
+import click
 import logging
 import glob
 import os
@@ -16,11 +17,13 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def process(path, name, file_name, file_out_name, container, **kwargs):
+def process(
+    rev, lsd, path, name, file_name, file_out_name, container, tryrun=False, **kwargs
+):
     in_file = glob.glob(os.path.join(path, file_name))
     if not in_file:
         logger.info(f"Found 0 {name} files in {path} (Expected 1).")
-        return
+        return 0
     elif len(in_file) > 1:
         logger.info(f"Found {len(in_file)} {name} files in {path} (Expected 1).")
         sys.exit(1)
@@ -36,68 +39,97 @@ def process(path, name, file_name, file_out_name, container, **kwargs):
 
     full_out_dir = os.path.join(out_dir, rev, str(lsd))
 
-    Path(full_out_dir).mkdir(parents=True, exist_ok=True)
+    if not tryrun:
+        Path(full_out_dir).mkdir(parents=True, exist_ok=True)
     out_file = os.path.join(full_out_dir, f"{file_out_name}_{lsd}.h5")
     if os.path.isfile(out_file) and not force:
         logger.debug(
             f"Skipping {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
         )
-        return
+        return 0
     logger.info(
         f"Processing {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
     )
 
-    rm = container.from_file(in_file, **kwargs)
-    rm.to_disk(out_file)
+    if not tryrun:
+        rm = container.from_file(in_file, **kwargs)
+        rm.to_disk(out_file)
+    return 1
 
 
-rev_dirs = glob.glob(os.path.join(d, "rev_*"))
-for rev_dir in rev_dirs:
-    if not os.path.isdir(rev_dir):
-        logger.debug(f"Skipping {rev_dir} because it's not a (revision) directory.")
-        continue
-    rev = os.path.split(rev_dir)[-1]
+@click.command()
+@click.option(
+    "--tryrun/--notryrun",
+    help="Just print number of files that would get processed.",
+    default=False,
+    show_default=True,
+)
+def run(tryrun):
+    rev_dirs = glob.glob(os.path.join(d, "rev_*"))
+    processed_total = 0
+    for rev_dir in rev_dirs:
+        if not os.path.isdir(rev_dir):
+            logger.debug(f"Skipping {rev_dir} because it's not a (revision) directory.")
+            continue
+        rev = os.path.split(rev_dir)[-1]
 
-    lsd_dirs = sorted(glob.glob(os.path.join(rev_dir, "*")))
+        lsd_dirs = sorted(glob.glob(os.path.join(rev_dir, "*")))
 
-    index = {}
-    for lsd_dir in lsd_dirs:
-        try:
-            lsd = int(os.path.basename(lsd_dir))
-        except ValueError as err:
-            logger.debug(f"Skipping dir {lsd_dir}: {err}")
-        else:
-            if rev not in index:
-                index[rev] = {}
-            if lsd in index[rev]:
-                logger.error(f"Tried to add {lsd} twice")
+        index = {}
+        for lsd_dir in lsd_dirs:
+            try:
+                lsd = int(os.path.basename(lsd_dir))
+            except ValueError as err:
+                logger.debug(f"Skipping dir {lsd_dir}: {err}")
+            else:
+                if rev not in index:
+                    index[rev] = {}
+                if lsd in index[rev]:
+                    logger.error(f"Tried to add {lsd} twice")
+                    sys.exit(1)
+
+                ringmap_freqs = slice(399, 477, 969)
+                ringmap_pols = [0, 3]
+                processed_total = processed_total + process(
+                    rev,
+                    lsd,
+                    lsd_dir,
+                    "ringmap",
+                    "ringmap_lsd_*.h5",
+                    f"ringmap_validation_freqs_lsd",
+                    ccontainers.RingMap,
+                    freq_sel=ringmap_freqs,
+                    pol_sel=ringmap_pols,
+                )
+                processed_total = processed_total + process(
+                    rev,
+                    lsd,
+                    lsd_dir,
+                    "ringmap_intercyl",
+                    "ringmap_intercyl_lsd_*.h5",
+                    f"ringmap_intercyl_validation_freqs_lsd",
+                    ccontainers.RingMap,
+                    freq_sel=ringmap_freqs,
+                    pol_sel=ringmap_pols,
+                )
+                processed_total = processed_total + process(
+                    rev,
+                    lsd,
+                    lsd_dir,
+                    "sensitivity",
+                    "sensitivity_lsd_*.h5",
+                    f"sensitivity_validation_lsd",
+                    containers.SystemSensitivity,
+                    pol_sel=[0, 2],
+                )
+
+        if tryrun:
+            print(f"Would have processed {processed_total} files.")
+            if processed_total == 0:
                 sys.exit(1)
+        else:
+            print(f"Processed {processed_total} files.")
 
-            ringmap_freqs = slice(399, 477, 969)
-            ringmap_pols = [0, 3]
-            process(
-                lsd_dir,
-                "ringmap",
-                "ringmap_lsd_*.h5",
-                f"ringmap_validation_freqs_lsd",
-                ccontainers.RingMap,
-                freq_sel=ringmap_freqs,
-                pol_sel=ringmap_pols,
-            )
-            process(
-                lsd_dir,
-                "ringmap_intercyl",
-                "ringmap_intercyl_lsd_*.h5",
-                f"ringmap_intercyl_validation_freqs_lsd",
-                ccontainers.RingMap,
-                freq_sel=ringmap_freqs,
-                pol_sel=ringmap_pols,
-            )
-            process(
-                lsd_dir,
-                "sensitivity",
-                "sensitivity_lsd_*.h5",
-                f"sensitivity_validation_lsd",
-                containers.SystemSensitivity,
-                pol_sel=[0, 2],
-            )
+
+if __name__ == "__main__":
+    run()
--- scripts/val_preprocess.sh
+++ scripts/val_preprocess.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#SBATCH --account=rpp-krs
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1 # number of MPI processes
-#SBATCH --cpus-per-task=1 # number of OpenMP processes
-#SBATCH --mem=4G # memory per node
-#SBATCH --time=0-04:00:00
-#SBATCH --job-name=chp/validation-preprocessing
-
 source /project/rpp-krs/chime/chime_processed/daily/rev_02/venv/bin/activate
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+set -e
+python /home/rickn/chime_pipeline/code/bondia/scripts/val_preprocess.py --tryrun &> /project/rpp-krs/chime/chime_processed/validation_preprocess/jobout.log
 
-srun python /home/rickn/chime_pipeline/code/bondia/scripts/val_preprocess.py &> /project/rpp-krs/chime/chime_processed/validation_preprocess/jobout.log
+sbatch /home/rickn/chime_pipeline/code/bondia/scripts/val_preprocess.sbatch
