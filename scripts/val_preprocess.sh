e1e8f6e67c32c3e10dfbd28ee58df5941a77039b
Rick Nitsche <rick@phas.ubc.ca>
2021-03-30 16:44:32 -0700

    val_preprocess: only submit if there are no jobs already in slurm queue

--- scripts/val_preprocess.sh
+++ scripts/val_preprocess.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 source /project/rpp-krs/chime/chime_processed/daily/rev_02/venv/bin/activate
 
+# Only submit if no slurm job already queued.
+JOB_NAME="chp/validation"
+squeue -u $USER | grep $JOB_NAME
+if [ ${PIPESTATUS[1]} -eq 0 ]
+then
+  echo "There already is a slurm job with name $JOB_NAME"
+  exit 1
+fi
+
+# Only submit if there are files waiting to be processed.
 set -e
 python /home/rickn/chime_pipeline/code/bondia/scripts/val_preprocess.py --tryrun &> /project/rpp-krs/chime/chime_processed/validation_preprocess/jobout.log
 
