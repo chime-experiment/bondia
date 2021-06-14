0e4e576593a412ac698c8bf8b71b8d602c499889
Rick Nitsche <rick@phas.ubc.ca>
2021-06-14 13:43:44 -0700

    use pathlib and call it dryrun

--- scripts/val_preprocess.py
+++ scripts/val_preprocess.py
@@ -1,7 +1,5 @@
 import click
 import logging
-import glob
-import os
 import sys
 
 from pathlib import Path
@@ -9,7 +7,7 @@ from pathlib import Path
 from ch_pipeline.core import containers as ccontainers
 from draco.core import containers
 
-d = "/project/rpp-chime/chime/chime_processed/daily"
+dir = "/project/rpp-chime/chime/chime_processed/daily"
 out_dir = "/project/rpp-chime/chime/chime_processed/validation_preprocess"
 force = False
 
@@ -18,9 +16,9 @@ logging.basicConfig(level=logging.INFO)
 
 
 def process(
-    rev, lsd, path, name, file_name, file_out_name, container, tryrun=False, **kwargs
+    rev, lsd, path, name, file_name, file_out_name, container, dryrun=False, **kwargs
 ):
-    in_file = glob.glob(os.path.join(path, file_name))
+    in_file = path.glob(file_name)
     if not in_file:
         logger.info(f"Found 0 {name} files in {path} (Expected 1).")
         return 0
@@ -30,19 +28,19 @@ def process(
 
     in_file = in_file[0]
 
-    lsd_from_filename = int(os.path.splitext(os.path.basename(in_file))[0][-4:])
+    lsd_from_filename = int(in_file.stem[-4:])
     if lsd != lsd_from_filename:
         logger.error(
             f"Found file for LSD {lsd} when expecting LSD {lsd_from_filename}: {in_file}"
         )
         sys.exit(1)
 
-    full_out_dir = os.path.join(out_dir, rev, str(lsd))
+    full_out_dir = Path(out_dir) / rev / str(lsd)
 
-    if not tryrun:
+    if not dryrun:
         Path(full_out_dir).mkdir(parents=True, exist_ok=True)
-    out_file = os.path.join(full_out_dir, f"{file_out_name}_{lsd}.h5")
-    if os.path.isfile(out_file) and not force:
+    out_file = full_out_dir / f"{file_out_name}_{lsd}.h5"
+    if out_file.is_file() and not force:
         logger.debug(
             f"Skipping {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
         )
@@ -51,7 +49,7 @@ def process(
         f"Processing {rev} {name} file for lsd {lsd}: {in_file}, outfile: {out_file}_{lsd}.h5"
     )
 
-    if not tryrun:
+    if not dryrun:
         rm = container.from_file(in_file, **kwargs)
         rm.to_disk(out_file)
     return 1
@@ -59,26 +57,26 @@ def process(
 
 @click.command()
 @click.option(
-    "--tryrun/--notryrun",
+    "--dryrun/--nodryrun",
     help="Just print number of files that would get processed.",
     default=False,
     show_default=True,
 )
-def run(tryrun):
-    rev_dirs = glob.glob(os.path.join(d, "rev_*"))
+def run(dryrun):
+    rev_dirs = Path(dir).glob("rev_*")
     processed_total = 0
     for rev_dir in rev_dirs:
-        if not os.path.isdir(rev_dir):
+        if not rev_dir.is_dir():
             logger.debug(f"Skipping {rev_dir} because it's not a (revision) directory.")
             continue
-        rev = os.path.split(rev_dir)[-1]
+        rev = rev_dir.parts[-1]
 
-        lsd_dirs = sorted(glob.glob(os.path.join(rev_dir, "*")))
+        lsd_dirs = sorted(rev_dir.glob("*"))
 
         index = {}
         for lsd_dir in lsd_dirs:
             try:
-                lsd = int(os.path.basename(lsd_dir))
+                lsd = int(lsd_dir.name)
             except ValueError as err:
                 logger.debug(f"Skipping dir {lsd_dir}: {err}")
             else:
@@ -96,10 +94,11 @@ def run(tryrun):
                     lsd_dir,
                     "ringmap",
                     "ringmap_lsd_*.h5",
-                    f"ringmap_validation_freqs_lsd",
+                    "ringmap_validation_freqs_lsd",
                     ccontainers.RingMap,
                     freq_sel=ringmap_freqs,
                     pol_sel=ringmap_pols,
+                    dryrun=dryrun,
                 )
                 processed_total = processed_total + process(
                     rev,
@@ -107,10 +106,11 @@ def run(tryrun):
                     lsd_dir,
                     "ringmap_intercyl",
                     "ringmap_intercyl_lsd_*.h5",
-                    f"ringmap_intercyl_validation_freqs_lsd",
+                    "ringmap_intercyl_validation_freqs_lsd",
                     ccontainers.RingMap,
                     freq_sel=ringmap_freqs,
                     pol_sel=ringmap_pols,
+                    dryrun=dryrun,
                 )
                 processed_total = processed_total + process(
                     rev,
@@ -118,12 +118,13 @@ def run(tryrun):
                     lsd_dir,
                     "sensitivity",
                     "sensitivity_lsd_*.h5",
-                    f"sensitivity_validation_lsd",
+                    "sensitivity_validation_lsd",
                     containers.SystemSensitivity,
                     pol_sel=[0, 2],
+                    dryrun=dryrun,
                 )
 
-        if tryrun:
+        if dryrun:
             print(f"Would have processed {processed_total} files.")
             if processed_total == 0:
                 sys.exit(1)
