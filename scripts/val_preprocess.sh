e8a466ef189de047afee5ff4439e0c441c706838
anjakefala <anja.kefala@gmail.com>
2022-02-03 10:16:38 -0800

    feat: add REVISION as an argument to scripts

--- scripts/val_preprocess.sh
+++ scripts/val_preprocess.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-source /project/rpp-chime/chime/chime_processed/daily/rev_03/venv/bin/activate
+
+set -x
+
+CHIME="/project/rpp-chime/chime"
+PROCESSED="$CHIME"/chime_processed
+ENV="$CHIME"/chime_env
+
+REVISION="${REVISION:-$(ls "$PROCESSED"/daily | tail -n 1)}"
+source "$PROCESSED"/daily/"$REVISION"/venv/bin/activate
 
 # Only submit if no slurm job already queued.
 JOB_NAME="chp/validation"
+
 squeue -u $USER | grep $JOB_NAME
 if [ ${PIPESTATUS[1]} -eq 0 ]
 then
@@ -10,8 +19,8 @@ then
   exit 1
 fi
 
-# Only submit if there are files waiting to be processed.
 set -e
-python /project/rpp-chime/chime/chime_env/daily_validation_preprocessing/val_preprocess.py tryrun &> /project/rpp-chime/chime/chime_processed/validation_preprocess/jobout.log
+# Only submit if there are files waiting to be processed.
+python "$ENV"/daily_validation_preprocessing/val_preprocess.py tryrun &> "$PROCESSED"/validation_preprocess/jobout.log
 
-sbatch /project/rpp-chime/chime/chime_env/daily_validation_preprocessing/val_preprocess.sbatch
+sbatch REVISION="$REVISION" "$ENV"/daily_validation_preprocessing/val_preprocess.sbatch
