5fb6635af4c4d9ac1d43ba792d63903a504c223d
anjakefala <anja.kefala@gmail.com>
2022-02-10 16:53:40 -0800

    fix: --export variables to sbatch scripts

--- scripts/val_preprocess.sh
+++ scripts/val_preprocess.sh
@@ -23,4 +23,4 @@ set -e
 # Only submit if there are files waiting to be processed.
 python "$ENV"/daily_validation_preprocessing/val_preprocess.py dryrun &> "$PROCESSED"/validation_preprocess/jobout.log
 
-sbatch REVISION="$REVISION" "$ENV"/daily_validation_preprocessing/val_preprocess.sbatch
+sbatch --export=REVISION="$REVISION" "$ENV"/daily_validation_preprocessing/val_preprocess.sbatch
